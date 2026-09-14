aea4fc545e5fd2efa5ed0672a05db2fafa652f29
Uzair Ahmad <uzairahmad7734@gmail.com>
2026-09-14 16:26:54 +0500

    feat: add profile update functionality and cloudinary integration

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,11 +1,16 @@
-import { signup, login, logout } from "../controllers/auth.controller.js";
+import { signup, login, logout, updateProfile } from "../controllers/auth.controller.js";
 import express from "express";
+import { protectRoute } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
+router.put("/update-profile", protectRoute, updateProfile);
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+
+// check user is authenticated or not 
+router.get("/check", protectRoute, (req, res) => res.status(200).json(req.user));
 
 
 export default router;
