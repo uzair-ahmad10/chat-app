730e83d3b8bd4052ac014bbf960260003a38f718
Uzair Ahmad <uzairahmad7734@gmail.com>
2026-09-15 16:06:04 +0500

    frontens setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import ChatPage from "./pages/ChatPage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignupPage";
+import { useEffect } from "react";
+import { Navigate, Route, Routes } from "react-router";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden">
+      {/* DECORATORS - GRID BG & GLOW SHAPES */}
+      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
+      <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px]" />
+      <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
 
-export default App
+      <Routes>
+        <Route
+          path="/"
+          element={<ChatPage />}
+        />
+        <Route
+          path="/login"
+          element={<LoginPage />}
+        />
+        <Route
+          path="/signup"
+          element={<SignUpPage />}
+        />
+      </Routes>
+
+      <Toaster />
+    </div>
+  );
+};
+
+export default App;
